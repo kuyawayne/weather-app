b8686e17e97160048e98e073ace5172113295d27
Orlie Wayne A. Faustorilla <orliewayne.faustorilla@gmail.com>
2023-04-08 17:47:16 +0800

    updates

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { WeatherResponseProps } from "./weather";
 
 export const getWeather = async (address: string) => {
-  const response = await axios.get<WeatherResponseProps>(`http://localhost/weather/${address}`);
+  const response = await axios.get<WeatherResponseProps>(`https://weather-gpt-api.onrender.com/weather/${address}`);
 
   return response;
 };
