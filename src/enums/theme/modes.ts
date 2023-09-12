7139b09489ac7bac1b3704ee56a01af3db5f401f
Orlie Wayne Faustorilla <orliewayne.faustorilla@gmail.com>
2023-09-12 21:37:56 +0800

    refactored themes into ts enums

---
+++ src/enums/theme/modes.ts
@@ -0,0 +1,33 @@
+const enum ThemeModes {
+  light = "light",
+  dark = "dark",
+  cupcake = "cupcake",
+  bumblebee = "bumblebee",
+  emerald = "emerald",
+  corporate = "corporate",
+  synthwave = "synthwave",
+  retro = "retro",
+  cyberpunk = "cyberpunk",
+  valentine = "valentine",
+  halloween = "halloween",
+  garden = "garden",
+  forest = "forest",
+  aqua = "aqua",
+  lofi = "lofi",
+  pastel = "pastel",
+  fantasy = "fantasy",
+  wireframe = "wireframe",
+  black = "black",
+  luxury = "luxury",
+  dracula = "dracula",
+  cmyk = "cmyk",
+  autumn = "autumn",
+  business = "business",
+  acid = "acid",
+  lemonade = "lemonade",
+  night = "night",
+  coffee = "coffee",
+  winter = "winter"
+}
+
+export default ThemeModes;
--- src/state/reducers/theme.ts
+++ src/state/reducers/theme.ts
@@ -1,13 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
+import ThemeModes from "./../../enums/theme/modes.ts";
 
 interface InitialProps {
   themes: string[],
-  theme: "light" | "dark" | "cupcake" | "bumblebee" | "emerald" | "corporate" | "synthwave" | "retro" | "cyberpunk" | "valentine" | "halloween" | "garden" | "forest" | "aqua" | "lofi" | "pastel" | "fantasy" | "wireframe" | "black" | "luxury" | "dracula" | "cmyk" | "autumn" | "business" | "acid" | "lemonade" | "night" | "coffee" | "winter";
+  theme: ThemeModes;
 }
 
 const initial: InitialProps = {
   theme: "light",
-  themes: ["acid", "aqua", "autumn", "black", "bumblebee", "business", "coffee", "corporate", "cmyk", "cyberpunk", "dracula", "emerald", "fantasy", "forest", "garden", "halloween", "lemonade", "light", "lofi", "luxury", "night", "pastel", "retro", "synthwave", "valentine", "wireframe", "winter"],
+  themes: ThemeModes,
 };
 
 const themeSlice = createSlice({
